76869c2b3cd8df27a5e57d04b08c0b34a31f6a0a
mikezks <23002661+mikezks@users.noreply.github.com>
2022-11-22 20:40:11 +0100

    feat: flight service with http call

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../entities/flight';
+import { FlightService } from './flight.service';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,90 +14,20 @@ export class FlightSearchComponent implements OnInit {
   flights: Flight[] = [];
   selectedFlight: Flight | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
   }
 
   search(): void {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-
-    const params = new HttpParams()
-      .set('from', this.from)
-      .set('to', this.to);
-
-    const headers = new HttpHeaders()
-      .set('accept', 'application/json');
-
-    this.http
-      .get<Flight[]>(url, { params, headers })
+    this.flightService
+      .find(this.from, this.to)
       .subscribe(
         flights => this.flights = flights
       );
-
-    /* console.log({
-      from: this.from,
-      to: this.to
-    }); */
   }
 
   select(flight: Flight): void {
     this.selectedFlight = flight;
   }
 }
-
-
-
-
-const persons = [
-  {
-    id: 1,
-    firstname: 'Susi',
-    lastname: 'Sorglos',
-    regDate: new Date().toISOString()
-  },
-  {
-    id: 2,
-    firstname: 'Peter',
-    lastname: 'Sorgenvoll',
-    regDate: new Date().toISOString()
-  }
-];
-
-const articles = [
-  {
-    id: 80100,
-    title: 'WLAN Kabel',
-    vendor: 'Dummy Inc',
-    price: 10_000
-  }
-];
-
-const purchases = [
-  {
-    userId: 1,
-    articleID: 80100,
-    amount: 3
-  }
-];
-
-const viewModel = [
-  {
-    articleTitle: 'WLAN Kabel',
-    person: 'Susi Sorglos',
-    total: 30_000,
-    details: {
-      price: 10_000,
-      amount: 3
-    }
-  },
-  {
-    articleTitle: 'WLAN Kabel',
-    person: 'Peter Sorgenvoll',
-    total: 50_000,
-    details: {
-      price: 10_000,
-      amount: 5
-    }
-  }
-]
---
+++ src/app/flight-search/flight.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Flight } from '../entities/flight';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService {
+
+  constructor(private http: HttpClient) { }
+
+  find(from: string, to: string): Observable<Flight[]> {
+    const url = 'https://demo.angulararchitects.io/api/flight';
+
+    const params = new HttpParams()
+      .set('from', from)
+      .set('to', to);
+
+    const headers = new HttpHeaders()
+      .set('accept', 'application/json');
+
+    return this.http
+      .get<Flight[]>(url, { params, headers })
+  }
+}
