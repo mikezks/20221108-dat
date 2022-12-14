7ace98c684c950e74cbc5f214c124fb3a8f2d9cc
mikezks <23002661+mikezks@users.noreply.github.com>
2022-12-14 16:33:38 +0100

    feat: on init, on destroy

--- src/app/flight-booking/ui/flight-card/flight-card.component.ts
+++ src/app/flight-booking/ui/flight-card/flight-card.component.ts
@@ -1,18 +1,25 @@
 import { Flight } from './../../../entities/flight';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-flight-card',
   templateUrl: './flight-card.component.html',
   styleUrls: ['./flight-card.component.css']
 })
-export class FlightCardComponent {
+export class FlightCardComponent implements OnInit, OnDestroy {
   @Input() item: Flight | undefined;
   @Input() selected = false;
   @Output() selectedChange = new EventEmitter<boolean>();
 
+  ngOnInit(): void {
+    console.log('Flight Card INIT');
+  }
+
   toggleSelection(): void {
     this.selected = !this.selected;
     this.selectedChange.emit(this.selected);
+  }
+  ngOnDestroy(): void {
+    console.log('Flight Card DESTROY');
   }
 }
